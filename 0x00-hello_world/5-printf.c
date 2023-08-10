c0c3422bf976c477a8f15ac251181247cdf24f0f
KING-Effulgence <okaforekperekasunday@gmail.com>
2023-08-10 11:11:47 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: Alway 0 (success)
  */
-int main (void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,)
+printf("with proper grammar, but the outcome is a piece of art,);
 return (0);
 }
