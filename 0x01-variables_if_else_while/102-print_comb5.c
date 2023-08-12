29d7ad2a2049ad51d7c064757ae97f9934be52a1
KING-Effulgence <okaforekperekasunday@gmail.com>
2023-08-12 13:18:10 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,30 +7,31 @@
 
 int main(void)
 {
-	int number1 = 0;
-	int number2;
+	int i, j;
+	int a, b, c, d;
 
-	while (number1 <= 99)
+	for  (i = '0'; i <= 99; i++)
 	{
-	number2 = 0;
-	while (number2 <= 99)
-	{
-		if (number1 <= number2)
+		a = i / 10;
+		b = i % 10;
+		for (j = '0'; j <= 99; j++)
 		{
-		putchar(number1 / 10 + '0');
-		putchar(number1 % 10 + '0');
-		putchar(' ');
-		putchar(number2 / 10 + '0');
-		putchar(number2 % 10 + '0');
-			if (number1 != 99 || number2 != 99)
+			c = j / 10;
+			d = j % 10;
+			if (a < c || (a == c && b < d))
 			{
-			putchar(',');
-			putchar(' ');
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+				if (a != '9' && b != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
-		number2++;
-	}
-	number1++;
 	}
 	putchar('\n');
 	return (0);
