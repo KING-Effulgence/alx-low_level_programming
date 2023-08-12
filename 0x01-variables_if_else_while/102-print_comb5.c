36e74cfd3ad7ae597606488b474025ad029f1002
KING-Effulgence <okaforekperekasunday@gmail.com>
2023-08-11 19:04:57 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number1 = 0;
+	int number2;
+
+	while (number1 <= 99)
+	{
+	number2 = 0;
+	while (number2 <= 99)
+	{
+		if (number1 <= number2)
+		{
+		putchar(number1 / 10 + '0');
+		putchar(number1 % 10 + '0');
+		putchar(' ');
+		putchar(number2 / 10 + '0');
+		putchar(number2 % 10 + '0');
+			if (number1 != 99 || number2 != 99)
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+		number2++;
+	}
+	number1++;
+	}
+	putchar('\n');
+	return (0);
+}
