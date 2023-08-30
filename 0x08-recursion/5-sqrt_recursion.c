0b4c72bae7546a676b9409b18cc7e8ede3448aff
KING-Effulgence <okaforekperekasunday@gmail.com>
2023-08-30 15:57:01 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,31 @@
 #include "main.h"
-int actual_sqrt_recursion(int n, int a);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
+ * sqrt_a - returns the natural square root of a number
+ * @a: number to calculate the square root of
+ * @b: iteration
  *
+ * Return: the square root -1
+ */
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+	return (sqrt_a(a, b + 1));
+}
+
+/**
+ * _sqrt_recursion - recurses to find the natural square root of a number
+ * @n: number to calculate the square root of
  * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	return (actual_sqrt_recursion(n, 0));
-}
-
-/**
- * actual_sqrt_recursion - recurses to find the natural square root of a number
- * @n: number to calculate the square root of
- * @a: iterator
- *
- * Return: the resulting square root
- */
-int actual_sqrt_recursion(int n, int a)
-{
-	if (a * a > n)
-		return (-1);
-	if (a * 1 == n)
-		return (a);
-	return (actual_sqrt_recursion(n, a + 1));
+	return (sqrt_a(n, 0));
 }
